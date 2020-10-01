2f30600885b093486ec4750c1f45718562d545df
Kratosflare <55363044+Kratosflare@users.noreply.github.com>
2020-09-30 22:02:17 -0500

    Unimplemented elements
    I found the parts I forgot to save for the git

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -9,15 +9,33 @@ public class RigidbodyCharacterController : MonoBehaviour
 
     [SerializeField]
     private float maxSpeed = 2;
+
+    [SerializeField]
+    private PhysicMaterial stoppingPhysicMaterial, movingPhysicMaterial;
+
     private new Rigidbody rigidbody;
     private Vector2 input;
+    private new Collider collider;
+
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
     }
     private void FixedUpdate()
     {
+
         var inputDirection = new Vector3(input.x,0,input.y);
+
+        if (inputDirection.magnitude > 0)
+        {
+            collider.material = movingPhysicMaterial;
+        }
+        else
+        {
+            collider.material = stoppingPhysicMaterial;
+        }
+
         if (rigidbody.velocity.magnitude<maxSpeed)
         {
             rigidbody.AddForce(inputDirection * acclereationForce, ForceMode.Acceleration);
